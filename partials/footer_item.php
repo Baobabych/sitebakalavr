578024e1bfb8c93c13c389c93fd507af3b09ac9c
Павлій Олександра <150563054+Baobabych@users.noreply.github.com>
2025-05-13 18:05:27 +0200

    Create footer_item.php

---
+++ partials/footer_item.php
@@ -0,0 +1,31 @@
+ <div class="head">
+    <img src="img/key.png" alt="">
+    <h2>Musa Find</h2>
+</div>
+<h3>Питання? Баги? Рекомендації?</h3>
+<h3>Зв’яжись з нами!</h3>
+<div class="social">
+    <img src="img/facebook.png" alt="">
+    <img src="img/twitter.png" alt="">
+    <img src="img/instagram.png" alt="">
+</div>
+<div class="contacts">
+    <div class="contactsLine">
+        <img src="img/number.png" alt="">
+        <p>+380957030305</p>
+    </div>
+    <div class="contactsGroup">
+        <h4>Баги і пропозиції</h4>
+        <div class="contactsLine">
+            <img src="img/mail.png" alt="">
+            <p>musa.find@gmail.com</p>
+        </div>
+    </div>
+    <div class="contactsGroup">
+        <h4>Співпраця</h4>
+        <div class="contactsLine">
+            <img src="img/mail.png" alt="">
+            <p>team_musa.find@gmail.com</p>
+        </div>
+    </div>
+</div>
